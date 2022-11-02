6fd8f04ea16faac1edb1608e2428a75265eadb99
larson cop <97100495@st.deltion.nl>
2022-11-02 14:40:27 +0100

    Pizzeria

---
+++ Pizza.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="NL">
+<head>
+<title>Pizza Website</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+
+<h3 class="pizzeria">Larson's Pizzeria</h3>
+<p 
+
+<form method="POST">
+
+<!-- tabel data -->
+<table align="center" cellpadding="5">
+<tr>
+    <td>
+<!-- gegevens -->
+    <p>
+        <label>Naam:</label>
+        <input type="text" name="naam" class="tabel">
+    </p>
+    <p>
+        <label>Adres:</label>
+        <input type="text" name="adres" class="tabel">
+    </p>
+    <p>
+        <label>Postcode:</label>
+        <input type="text" name="postcode" class="tabel">
+    </p>
+    <p>
+        <label>Plaats:</label>
+        <input type="text" name="plaats" class="tabel">
+    </p>
+    <p>
+        <input type="date" value="2022-11-02"/>
+        <input type="time" id="appt" name="appt"
+               min="17:00" max="24  :00" required>
+
+    </p>
+    <p>
+        <select>
+        <option value="nee">Ja</option>
+        <option value="nee">Nee</option>
+        
+
+        </select>
+    </p>
+</body>
+</html>
+
