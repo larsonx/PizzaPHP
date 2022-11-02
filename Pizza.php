4427fe53fd262f072f3ef8330ee783dd9201e9ca
larson cop <97100495@st.deltion.nl>
2022-11-02 16:40:44 +0100

    y

--- Pizza.php
+++ Pizza.php
@@ -11,43 +11,47 @@
 <h3 class="pizzeria">Larson's Pizzeria</h3>
 <p 
 
-<form method="POST">
+<for method="POST">
 
 <!-- tabel data -->
 <table align="center" cellpadding="5">
 <tr>
     <td>
 <!-- gegevens -->
+<h3 class="persoonlijk">Persoons gegevens</h3>
     <p>
         <label>Naam:</label>
-        <input type="text" name="naam" class="tabel">
+        <input type="text" name="naam" class="textarea" required>
     </p>
     <p>
         <label>Adres:</label>
-        <input type="text" name="adres" class="tabel">
+        <input type="text" name="adres" class="textarea" required>
     </p>
     <p>
         <label>Postcode:</label>
-        <input type="text" name="postcode" class="tabel">
+        <input type="text" name="postcode" class="textarea" required>
     </p>
     <p>
         <label>Plaats:</label>
-        <input type="text" name="plaats" class="tabel">
+        <input type="text" name="plaats" class="textarea" required>
     </p>
-    <p>
-        <input type="date" value="2022-11-02"/>
-        <input type="time" id="appt" name="appt"
-               min="17:00" max="24  :00" required>
-
-    </p>
-    <p>
+   
+    <label>Bezorgen of afhalen:
         <select>
-        <option value="nee">Ja</option>
-        <option value="nee">Nee</option>
-        
+        <option value=""disabled selected hidden>Maak uw Keuze</option>
+        <option value="bezorgen">Bezorgen</option>
+        <option value="afhalen">Afhalen</option>
+
 
         </select>
+    <p>
+        <input class="datum" type="date" value="2022-11-02"/>
+        <input type="time" id="appt" name="appt"
+               min="17:00" max="24  :00" required>    
+        <button class="btn">Bestel</button>
     </p>
+           
+</form>
 </body>
 </html>
 
