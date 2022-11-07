2340702282cb7bb4c06e4525be5511639d8e29ab
larson cop <97100495@st.deltion.nl>
2022-11-07 12:48:23 +0100

    UPDATE

--- index.php
+++ index.php
@@ -1,19 +1,19 @@
 <?php
 
-$p1 = $_POST['quantity1'];
-$p2 = $_POST['quantity2'];
-$p3 = $_POST['quantity3'];
-$p4 = $_POST['quantity4'];
-$p5 = $_POST['quantity5'];
-$m = 15;
-$b = 5;
-$m = $_POST['quantity1'];
-$b = $_POST['quantity1'];
+
 
 if (isset($_POST['submit'])){ 
      
-    $cost=$p1 * $p2;
-    echo "$p1 * $p2 = ".$cost;
+    $p1 = $_POST['quantity1']* 12.50;
+    $p2 = $_POST['quantity2']* 12.50;
+    $p3 = $_POST['quantity3']* 13.95;
+    $p4 = $_POST['quantity4']* 14.50;
+    $p5 = $_POST['quantity5']* 11.50;
+    
+
+    
+    $cost=$p1 + $p2;
+    echo "$cost";
 
     
 
