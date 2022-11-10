052578c4face4e3969f33bb00f8de5fdc28ad835
larson cop <97100495@st.deltion.nl>
2022-11-10 09:41:19 +0100

    update

--- index.php
+++ index.php
@@ -3,17 +3,31 @@
 
 
 if (isset($_POST['submit'])){ 
+    $dag = date('l');
     $aantal1 = $_POST['aantal1']* 12.50;
     $aantal2 = $_POST['aantal2']* 12.50;
     $aantal3 = $_POST['aantal3']* 13.95;
     $aantal4 = $_POST['aantal4']* 14.50;
     $aantal5 = $_POST['aantal5']* 11.50;
-    $korting = 0.15;
-    $bezorg  = 5;
+    $bezorgkosten  = 5;
     $maandag = 0;
-    $vrijdag = 0; 
+
+if ($dag == 'Monday'){
+    $aantal1 = $_POST['aantal1']* 7.50;
+    $aantal2 = $_POST['aantal2']* 7.50;
+    $aantal3 = $_POST['aantal3']* 7.50;
+    $aantal4 = $_POST['aantal4']* 7.50;
+    $aantal5 = $_POST['aantal5']* 7.50;
+  
+}
+if ($dag == 'Friday'){
+
     
+}
+     
     
+
+
 
    
     
---
+++ oefen.php
@@ -0,0 +1,6 @@
+<?php
+$discount_amount = 15;
+$total = ($discount_amount/100);
+
+echo $total;
+?>
