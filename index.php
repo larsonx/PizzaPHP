cac7c1bc8380b61e7310a2034396c60c508aab31
larson cop <97100495@st.deltion.nl>
2022-11-10 13:08:07 +0100

    update

--- index.php
+++ index.php
@@ -7,22 +7,23 @@ if (isset($_POST['submit'])){
     $aantal3 = $_POST['aantal3']* 13.95;
     $aantal4 = $_POST['aantal4']* 14.50;
     $aantal5 = $_POST['aantal5']* 11.50;
-    $total = $aantal1 + $aantal2 + $aantal3 + $aantal4 + $aantal5;
-    $total = $total - ($total * (15/100));
+    $korting = $aantal1 + $aantal2 + $aantal3 + $aantal4 + $aantal5;
+    $korting = $korting - ($korting * (15/100));
     $bezorgkosten  = 5;
 
-if ($dag == 'Monday'){
+
+    
+if ($dag == 'Thursday'){
     $aantal1 = $_POST['aantal1']* 7.50;
     $aantal2 = $_POST['aantal2']* 7.50;
     $aantal3 = $_POST['aantal3']* 7.50;
     $aantal4 = $_POST['aantal4']* 7.50;
     $aantal5 = $_POST['aantal5']* 7.50;
-    echo $aantal1 + $aantal2 + $aantal3 + $aantal4 + $aantal5;
-    
+
 }
 if ($dag == 'Friday' && $total > 20){
-    $total = $total - ($total * (15/100));
-    echo $total;
+    $korting = $korting - ($korting * (15/100));
+  
 }
 
 
