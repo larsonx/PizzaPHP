9152296070ce76c725dfd45935714f2a1bdb1284
larson cop <97100495@st.deltion.nl>
2022-11-10 15:54:52 +0100

    help

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 if (isset($_POST['submit'])){ 
     $dag = date('l');
+
     $aantal1 =    $_POST['aantal1']* 12.50;
     $aantal2 =    $_POST['aantal2']* 12.50;
     $aantal3 =    $_POST['aantal3']* 13.95;
@@ -9,7 +10,7 @@ if (isset($_POST['submit'])){
     $aantal5 =    $_POST['aantal5']* 11.50;
     $korting = $aantal1 + $aantal2 + $aantal3 + $aantal4 + $aantal5;
     $korting = $korting - ($korting * (15/100));
-   
+    
 
 if ($dag == 'Monday'){
     $aantal1 = $_POST['aantal1']* 7.50;
@@ -23,10 +24,5 @@ if ($dag == 'Friday' && $korting > 20){
     $korting = $korting - ($korting * (15/100));
     echo "€" . number_format($korting,2, ','); 
 }
-
-
-
-
-
 }
 ?>
