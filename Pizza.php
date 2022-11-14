d1d8e62bb1964d6f43c125a84be1a0f4356da01d
larson cop <97100495@st.deltion.nl>
2022-11-14 08:51:43 +0100

    pl

--- Pizza.php
+++ Pizza.php
@@ -9,8 +9,6 @@ if (isset($_POST['submit'])){
     $aantal5 =    $_POST['aantal5'];
     $korting = 0;
     $totaal_prijs = 0;
-    $bezorgen = 5;
-    $select = $_POST['select'];
     if ($dag == 'Monday'){
         $totaal_prijs = ($aantal1 + $aantal2 + $aantal3 + $aantal4 + $aantal5)* 7.50;    
 
@@ -23,13 +21,35 @@ if (isset($_POST['submit'])){
         if ($dag == 'Friday' && $totaal_prijs > 20){
             $korting = $totaal_prijs * 0.15;
 
-            
     }
 
 $totaal_prijs = $totaal_prijs - $korting;
 echo "vrijdag korting €" . number_format($korting,2, ',')."<br>";
 echo "Te betalen: €" . number_format ($totaal_prijs,2, ',')."<br>";
-echo "Totaal: €" . number_format($totaal_prijs,2,',')."<br>";
+echo "Totaal: €" . number_format($totaal_prijs,2, ',')."<br>";
 }
-}
+die;
+
+
+
+    
+   
+    $bezorgen = $_POST['select'];
+
+    if($bezorgen == "bezorgen"){
+        
+       echo "bezorgen €" . number_format($aantal1 + $aantal2 + $aantal3 + $aantal4 + $aantal5 +5,2, ',');
+    }else{
+       echo "afhalen €" . number_format($aantal1 + $aantal2 + $aantal3 + $aantal4 + $aantal5 ,2, ',');
+    } 
+
+    
+  
+  
+
+    
+    
+    }
+
+ 
 ?>
