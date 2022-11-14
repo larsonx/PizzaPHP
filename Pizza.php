3a2beed03af888f4ff1068dadd02bb6dba4f485c
larson cop <97100495@st.deltion.nl>
2022-11-14 11:27:10 +0100

    update

--- Pizza.php
+++ Pizza.php
@@ -2,29 +2,33 @@
 
 if (isset($_POST['submit'])){ 
     $dag = date('l');
-    $aantal1 =    $_POST['aantal1'];
-    $aantal2 =    $_POST['aantal2'];
-    $aantal3 =    $_POST['aantal3'];
-    $aantal4 =    $_POST['aantal4'];
-    $aantal5 =    $_POST['aantal5'];
+    $pizza1 =    $_POST['aantal1'];
+    $pizza2 =    $_POST['aantal2'];
+    $pizza3 =    $_POST['aantal3'];
+    $pizza4 =    $_POST['aantal4'];
+    $pizza5 =    $_POST['aantal5'];
     $korting = 0;
     $totaal_prijs = 0;
-    $bezorgen = $_POST['bezorgen'];
-
+    $bezorgen = $_POST['select'];
+    $bezorgkosten = 5.00;
     
+    if($bezorgen == 'bezorgen'){
+        echo "bezorgkosten: €5,00<br>\n";
+        
+    }
     
-    if ($dag == 'Monday' ){
-    $totaal_prijs = ($aantal1 + $aantal2 + $aantal3 + $aantal4 + $aantal5)* 7.50;  
-
+    if ($dag == 'Monday'){
+    $totaal_prijs = ($pizza1 + $pizza2 + $pizza3 + $pizza4 + $pizza5)* 7.50;  
     echo "maandag actie:  " . number_format($totaal_prijs,2, ',')."<br>";
-    echo "Te betalen: €" . number_format($totaal_prijs,2, ',')."<br>";    
+    echo "Te betalen: €" . number_format($totaal_prijs+5,2, ',')."<br>";    
 
-    }else {
-        $totaal_prijs += $aantal1*12.50;
-        $totaal_prijs += $aantal2*12.50;
-        $totaal_prijs += $aantal3*13.95;
-        $totaal_prijs += $aantal4*11.50;
-        $totaal_prijs += $aantal5*14.50; 
+    }else{
+        
+        $totaal_prijs += $pizza1*12.50;
+        $totaal_prijs += $pizza2*12.50;
+        $totaal_prijs += $pizza3*13.95;
+        $totaal_prijs += $pizza4*11.50;
+        $totaal_prijs += $pizza5*14.50; 
     
         if ($dag == 'Friday' && $totaal_prijs > 20){
             $korting = $totaal_prijs * 0.15;
