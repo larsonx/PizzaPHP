cb521c66dad839475042b5fd283f679305ea824c
larson cop <97100495@st.deltion.nl>
2022-11-15 10:26:52 +0100

    update

--- Pizza.php
+++ Pizza.php
@@ -13,12 +13,14 @@ if (isset($_POST['submit'])){
     $bezorgkosten = 5.00;
     $x=0;
    
-    function bezorgkosten($x, $bezorgen, $pizza1,$pizza2,$pizza3,$pizza4,$pizza5){
+    function bezorgkostenmaandag($x, $bezorgen, $pizza1,$pizza2,$pizza3,$pizza4,$pizza5){
         
         if($bezorgen == 'bezorgen'){
             $totaalprijs = $x;
-         echo   $x = ($pizza1 + $pizza2 + $pizza3 + $pizza4 + $pizza5)* 7.50 +5;     
-       
+            $x = ($pizza1 + $pizza2 + $pizza3 + $pizza4 + $pizza5)* 7.50 +5;     
+         echo "Maandag zijn alle Pizza's €7,50"."<br>";
+         echo "Bezorgkosten €5,00"."<br>";   
+         echo "Totaal:  "."€". number_format($x,2, ',')."<br>";;
        }elseif($bezorgen == 'afhalen'){
         echo   $x = ($pizza1 + $pizza2 + $pizza3 + $pizza4 + $pizza5)* 7.50; 
        }
@@ -27,10 +29,9 @@ if (isset($_POST['submit'])){
    
      
     if ($dag == 'Monday'){
-    $totaal_prijs = ($pizza1 + $pizza2 + $pizza3 + $pizza4 + $pizza5)* 7.50; 
- 
-    echo "maandag actie:  " . number_format($totaal_prijs,2, ',')."<br>";
-    echo "Totaal: €" . number_format($totaal_prijs,2, ',')."<br>";    
+    $totaal_prijs = ($pizza1 + $pizza2 + $pizza3 + $pizza4 + $pizza5)* 7.50;    
+    bezorgkostenmaandag($x, $bezorgen,$pizza1,$pizza2,$pizza3,$pizza4,$pizza5);    
+    
     }else{   
         $totaal_prijs += $pizza1*12.50;
         $totaal_prijs += $pizza2*12.50;
@@ -38,18 +39,18 @@ if (isset($_POST['submit'])){
         $totaal_prijs += $pizza4*11.50;
         $totaal_prijs += $pizza5*14.50; 
     
-        if ($dag == 'Friday' && $totaal_prijs > 20){
+        if ($dag == 'Tuesday' && $totaal_prijs > 20){
             $korting = $totaal_prijs * 0.15;
-            
+               
             $totaal_prijs = $totaal_prijs - $korting;
             echo "vrijdag korting €" . number_format($korting,2, ',')."<br>";
-            
+            echo "Totaal: €" . number_format($totaal_prijs,2, ',')."<br>";
 
     }
     }
-    if ($dag == 'Tuesday'&& 'Wednesday' && 'Thursday'){
+    if ($dag == ''&& 'Wednesday' && 'Thursday'){
         echo "Totaal: €" . number_format($totaal_prijs,2, ',')."<br>";
-        bezorgkosten($x, $bezorgen,$pizza1,$pizza2,$pizza3,$pizza4,$pizza5);
+        
     }
     
     }
--- oefen.php
+++ oefen.php
@@ -3,15 +3,17 @@ $te = "maken voor bezorgkosten";
 
 
 
-function bezorgkosten($x, $bezorgen){
+function bezorgkostenmaandag($x, $bezorgen, $pizza1,$pizza2,$pizza3,$pizza4,$pizza5){
         
-   if($bezorgen == 'bezorgen'){
-       $totaalprijs = $x;
-       $x = ($pizza1 + $pizza2 + $pizza3 + $pizza4 + $pizza5)* 7.50 +5;     
-  
-  }elseif($bezorgen == 'afhalen'){
- "xd  ";
-  }
+  if($bezorgen == 'bezorgen'){
+      $totaalprijs = $x;
+      $x = ($pizza1 + $pizza2 + $pizza3 + $pizza4 + $pizza5)* 7.50 +5;     
+   echo "Maandag zijn alle Pizza's €7,50"."<br>";
+   echo "Bezorgkosten €5,00"."<br>";   
+   echo "Totaal:  "."€". number_format($x,2, ',')."<br>";;
+ }elseif($bezorgen == 'afhalen'){
+  echo   $x = ($pizza1 + $pizza2 + $pizza3 + $pizza4 + $pizza5)* 7.50; 
+ }
 
 }
 
