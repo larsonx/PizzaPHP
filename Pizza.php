1421c37bb4a069f110cb9f723c5993bf17038236
larson cop <97100495@st.deltion.nl>
2022-11-22 08:11:19 +0100

    Update Pizza.php

--- Pizza.php
+++ Pizza.php
@@ -1,6 +1,7 @@
 <?php
 
-if (isset($_POST['submit'])){ 
+if (isset($_POST['submit']))
+{ 
     $dag            =date('l');
     $naam           =$_POST['naam'];
     $adres          =$_POST['adres'];
@@ -32,71 +33,84 @@ if (isset($_POST['submit'])){
         echo "Aantal hawai: ".$_POST['hawai']."<br>";
         echo "Aantal quattroformaggi: ".$_POST['quattroformaggi']."<br>"."<br>";
 
-        function bezorgkostenmaandag($x, $bezorgen, $pizza1,$pizza2,$pizza3,$pizza4,$pizza5){
+        function bezorgkostenmaandag($x, $bezorgen, $pizza1,$pizza2,$pizza3,$pizza4,$pizza5)
+    {
         
-        if($bezorgen == 'bezorgen'){
+        if($bezorgen == 'bezorgen')
+        {
             $totaalprijs = $x;
             $x = ($pizza1 + $pizza2 + $pizza3 + $pizza4 + $pizza5)* 7.50 +5;     
             echo "Maandag zijn alle Pizza's €7,50"."<br>";
             echo "Bezorgkosten €5,00"."<br>";   
             echo "Totaal:  "."€". number_format($x,2, ',')."<br>";;
-    }   elseif($bezorgen == 'afhalen'){
+        }   elseif($bezorgen == 'afhalen')
+        {
             echo   $x = ($pizza1 + $pizza2 + $pizza3 + $pizza4 + $pizza5)* 7.50; 
-    }
+        }
      
     }
    
-        function vrijdag($bezorgen,$totaal_prijs,$korting){
-        if($bezorgen == 'bezorgen'){
+        function vrijdag($bezorgen,$totaal_prijs,$korting)
+    {
+        if($bezorgen == 'bezorgen')
+        {
           echo "vrijdag 15% korting  -€" . number_format($korting,2, ',')."<br>";
           echo "Bezorgkosten: €5,00"."<br>";
           echo"Totaal: €" . number_format($totaal_prijs+5,2, ',')."<br>";
         
-    }   elseif($bezorgen == 'afhalen'){
+        }   elseif($bezorgen == 'afhalen')
+        {
           echo "vrijdag 15% korting -€" . number_format($korting,2, ',')."<br>";
           echo "Totaal: €" . number_format($totaal_prijs,2, ',')."<br>";
           
-    }
+        }
     }
 
-        function diwodo($bezorgen,$totaal_prijs){
-        if($bezorgen == 'bezorgen'){
+        function diwodo($bezorgen,$totaal_prijs)
+    {
+        if($bezorgen == 'bezorgen')
+        {
               
               echo "Bezorgkosten: €5,00"."<br>";
               echo"Subtotaal: €" . number_format($totaal_prijs,2, ',')."<br>";
               echo"Totaal: €" . number_format($totaal_prijs+5,2, ',')."<br>";
             
-    }   elseif($bezorgen == 'afhalen'){
+        }   elseif($bezorgen == 'afhalen')
+        {
               
               echo "Totaal: €" . number_format($totaal_prijs,2, ',')."<br>";
               
-    }
+        }
     }
 
-        if ($dag == 'Monday'){
+        if ($dag == 'Monday')
+        {
             $totaal_prijs = ($pizza1 + $pizza2 + $pizza3 + $pizza4 + $pizza5)* 7.50;    
             bezorgkostenmaandag($x, $bezorgen,$pizza1,$pizza2,$pizza3,$pizza4,$pizza5);    
     
-    }   else{   
+        }   else
+        {   
             $totaal_prijs += $pizza1*12.50;
             $totaal_prijs += $pizza2*12.50;
             $totaal_prijs += $pizza3*13.95;
             $totaal_prijs += $pizza4*11.50;
             $totaal_prijs += $pizza5*14.50; 
     
-        if ($dag == 'Friday' && $totaal_prijs > 20){
-            $korting = $totaal_prijs * 0.15;     
-            $totaal_prijs = $totaal_prijs - $korting;
-            vrijdag($bezorgen,$totaal_prijs,$korting); 
+            if ($dag == 'Friday' && $totaal_prijs > 20)
+            {
+                $korting = $totaal_prijs * 0.15;     
+                $totaal_prijs = $totaal_prijs - $korting;
+                vrijdag($bezorgen,$totaal_prijs,$korting); 
             
             
-    }
-    }
-        if ($dag == 'Tuesday'&& 'Wednesday' && 'Thursday'){
-        diwodo($bezorgen,$totaal_prijs);
-    }
+            }
+        }
+        if ($dag == 'Tuesday'&& 'Wednesday' && 'Thursday')
+        {
+            diwodo($bezorgen,$totaal_prijs);
+        }
     
-    }
+}
     
  
 ?>
